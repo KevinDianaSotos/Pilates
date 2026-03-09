cea6bc36fffa8cdffac9af77381e27e52efdb6f1
Kevin Diana Sotos <Kevin Diana Sotos@DESKTOP-ICDSHOP>
2026-03-09 09:16:32 +0000

    Add PWA configuration

--- Frontend/pilates-app/src/main.ts
+++ Frontend/pilates-app/src/main.ts
@@ -4,11 +4,18 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appRouter } from './app/app.routes';
 import { authInterceptor } from './app/core/interceptors/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
+
 bootstrapApplication(AppComponent, {
   providers: [
     appRouter,
     provideHttpClient(
       withInterceptors([authInterceptor])
-    )
+    ),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(), // Solo se activa en producción
+      registrationStrategy: 'registerWhenStable:30000' // Espera 30s después de que la app esté estable
+    })
   ]
-})
+}).catch(err => console.error(err));
